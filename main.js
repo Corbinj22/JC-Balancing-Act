ce0ae96d5b68f88ed5933077ac849615888d0b8d
Justin Corbin <jcorbin2290@gmail.com>
2019-11-21 22:35:08 -0700

    clean up code formating

---
+++ main.js
@@ -0,0 +1,5 @@
+var banner = document.querySelector(".intro");
+document.querySelector(".close-button").addEventListener("click", closeBanner);
+function closeBanner(){
+  banner.style.display= "none";
+}
